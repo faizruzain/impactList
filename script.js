b2e1dc1d0d4354ace951210be6f8877f7a2951e5
Faiz Ruzain <faiz.faiz8383@gmail.com>
2023-10-22 17:23:23 +0700

    block number

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 function setImpact(res) {
   res = [...new Set(res)];
-  //return "Jumlah Site:" + res.length + "\n" + "2G:" + res.length + "NE" + "\n" + "3G:" + res.length + "NE" + "\n" + "4G:" + res.length + "NE" + "\n" + "Detail Site:" + "\n" + res; 
-  return "Jumlah Site:" + res.length + "\n" + "2G:" + res.length + "NE" + "\n" + "3G:" + res.length + "NE" + "\n" + "4G:" + res.length + "NE" + "\n" + "Detail Site:" + "\n" + "[" + res + "]";
+  //return "Jumlah Site:" + res.length + "\n" + "2G:" + res.length + "NE" + "\n" + "3G:" + res.length + "NE" + "\n" + "4G:" + res.length + "NE" + "\n" + "Detail Site:" + "\n" + res;
+  return (
+    "Jumlah Site:" +
+    res.length +
+    "\n" +
+    "2G:" +
+    res.length +
+    "NE" +
+    "\n" +
+    "3G:" +
+    res.length +
+    "NE" +
+    "\n" +
+    "4G:" +
+    res.length +
+    "NE" +
+    "\n" +
+    "Detail Site:" +
+    "\n" +
+    "[" +
+    res +
+    "]"
+  );
 }
 
 function parse() {
@@ -9,16 +30,15 @@ function parse() {
 
   if (!val) {
     document.getElementById("fixRes").value = "No input";
-
   } else {
     val = val.toUpperCase();
-    document.getElementById("fixRes").value = setImpact(val.match(/[A-Z]{3}\d{3}/g));
-
+    document.getElementById("fixRes").value = setImpact(
+      val.match(/[A-Z]{3}\d{3}/g)
+    );
   }
 
   document.getElementById("inlineRadio6").checked = false;
   document.getElementById("inlineRadio7").checked = false;
-
 }
 
 function report() {
@@ -28,7 +48,6 @@ function report() {
 
   if (val.length === 9) {
     document.getElementById("reportRes").value = "No input";
-
   } else {
     for (var i = 0; i < arr.length; i++) {
       if (arr[i].length === 8) {
@@ -36,58 +55,116 @@ function report() {
       } else {
         badArr.push(parseInt(arr[i].substring(7)));
       }
-
     }
 
-    let filteredArr = badArr.filter(item => item || item === 0);
+    let filteredArr = badArr.filter((item) => item || item === 0);
 
     let d = new Date();
     let date = d.toLocaleDateString("id-ID", {
-      "dateStyle": "medium"
+      dateStyle: "medium",
     });
     let time = d.toLocaleTimeString("id-ID", {
-      "timeStyle": "short"
+      timeStyle: "short",
     });
 
-    document.getElementById("reportRes").value = "**Reporting CSO, TANGGAL: " + date + " Pukul " + time + " WIB :**" + "\n" + "\n" + "**Tiket Service Tsel:**" + "\n" + "TREGx: <4Jam / >4Jam" + "\n" + "TREG1: " + filteredArr[1] + "/" + filteredArr[9] + "\n" + "TREG2: " + filteredArr[2] + "/" + filteredArr[10] + "\n" + "TREG3: " + filteredArr[3] + "/" + filteredArr[11] + "\n" + "TREG4: " + filteredArr[4] + "/" + filteredArr[12] + "\n" + "TREG5: " + filteredArr[5] + "/" + filteredArr[13] + "\n" + "TREG6: " + filteredArr[6] + "/" + filteredArr[14] + "\n" + "TREG7: " + filteredArr[7] + "/" + filteredArr[15] + "\n" + "**Total: " + filteredArr[8] + "/" + filteredArr[16] + "**" + "\n" + "\n" + "**Total Tiket Tsel " + (filteredArr[17] + filteredArr[8] + filteredArr[16]) + " Tiket : Terdiri dari (SALSIM : " + filteredArr[17] + " Tiket ; ONPROGRESS : " + (filteredArr[8] + filteredArr[16]) + " Tiket)**";
-
+    document.getElementById("reportRes").value =
+      "**Reporting CSO, TANGGAL: " +
+      date +
+      " Pukul " +
+      time +
+      " WIB :**" +
+      "\n" +
+      "\n" +
+      "**Tiket Service Tsel:**" +
+      "\n" +
+      "TREGx: <4Jam / >4Jam" +
+      "\n" +
+      "TREG1: " +
+      filteredArr[1] +
+      "/" +
+      filteredArr[9] +
+      "\n" +
+      "TREG2: " +
+      filteredArr[2] +
+      "/" +
+      filteredArr[10] +
+      "\n" +
+      "TREG3: " +
+      filteredArr[3] +
+      "/" +
+      filteredArr[11] +
+      "\n" +
+      "TREG4: " +
+      filteredArr[4] +
+      "/" +
+      filteredArr[12] +
+      "\n" +
+      "TREG5: " +
+      filteredArr[5] +
+      "/" +
+      filteredArr[13] +
+      "\n" +
+      "TREG6: " +
+      filteredArr[6] +
+      "/" +
+      filteredArr[14] +
+      "\n" +
+      "TREG7: " +
+      filteredArr[7] +
+      "/" +
+      filteredArr[15] +
+      "\n" +
+      "**Total: " +
+      filteredArr[8] +
+      "/" +
+      filteredArr[16] +
+      "**" +
+      "\n" +
+      "\n" +
+      "**Total Tiket Tsel " +
+      (filteredArr[17] + filteredArr[8] + filteredArr[16]) +
+      " Tiket : Terdiri dari (SALSIM : " +
+      filteredArr[17] +
+      " Tiket ; ONPROGRESS : " +
+      (filteredArr[8] + filteredArr[16]) +
+      " Tiket)**";
   }
-
 }
 
 function regTlkm(regTsel) {
   if (regTsel === "null") {
-    return regTelkom = "";
+    return (regTelkom = "");
   } else if (regTsel === "1" || regTsel === "2" || regTsel === "10") {
-    return regTelkom = "1";
+    return (regTelkom = "1");
   } else if (regTsel === "3") {
-    return regTelkom = "2";
+    return (regTelkom = "2");
   } else if (regTsel === "4") {
-    return regTelkom = "3";
+    return (regTelkom = "3");
   } else if (regTsel === "5") {
-    return regTelkom = "4";
+    return (regTelkom = "4");
   } else if (regTsel === "6" || regTsel === "7") {
-    return regTelkom = "5";
+    return (regTelkom = "5");
   } else if (regTsel === "8") {
-    return regTelkom = "6";
+    return (regTelkom = "6");
   } else if (regTsel === "9" || regTsel === "11") {
-    return regTelkom = "7";
+    return (regTelkom = "7");
   }
 }
 
 function radio() {
-  let val = document.getElementById("radio").value.replace(/\t/g, "").split("\n");
-  
-  val = val.filter((val) => {
-    return val
-  })
-  
+  let val = document
+    .getElementById("radio")
+    .value.replace(/\t/g, "")
+    .split("\n");
 
-  let val2 = []
-  if(val.length === 4) {
-    
-    for(var i=0; i<val.length; i++) {
-     val2.push(val[i].split(":"))      
+  val = val.filter((val) => {
+    return val;
+  });
+
+  let val2 = [];
+  if (val.length === 4) {
+    for (var i = 0; i < val.length; i++) {
+      val2.push(val[i].split(":"));
     }
   }
 
@@ -96,65 +173,62 @@ function radio() {
     regTsel = "";
   }
 
-  const regTelkom = regTlkm(regTsel)
+  const regTelkom = regTlkm(regTsel);
 
-  let radioBtn = document.querySelectorAll('.radio-aing')
+  let radioBtn = document.querySelectorAll(".radio-aing");
 
   function impact() {
     // let radioBtn = document.querySelectorAll('.radio-aing')
 
-    if(radioBtn[0].checked) {
-      return "DOWN"
-    } else if(radioBtn[1].checked) {
-      return "PACKETLOSS_QUALITY"
-    } else if(radioBtn[2].checked) {
-      return "FLICKER_QUALITY"
-    } else if(radioBtn[3].checked) {
-      return "PREVENTIVE"
+    if (radioBtn[0].checked) {
+      return "DOWN";
+    } else if (radioBtn[1].checked) {
+      return "PACKETLOSS_QUALITY";
+    } else if (radioBtn[2].checked) {
+      return "FLICKER_QUALITY";
+    } else if (radioBtn[3].checked) {
+      return "PREVENTIVE";
     } else {
-      return "???"
+      return "???";
     }
-    
   }
-  const service = impact()
+  const service = impact();
 
   function bast1AndType() {
-    let checkBoxes = document.querySelectorAll(".checkbox-aing")
+    let checkBoxes = document.querySelectorAll(".checkbox-aing");
 
-    if(checkBoxes[0].checked && checkBoxes[1].checked) {
-      return "GARANSI_PREMIUM"
-    } else if(checkBoxes[0].checked && !checkBoxes[1].checked) {
-      return "NOGARANSI_PREMIUM"
-    } else if(checkBoxes[1].checked){
-      return "GARANSI"
-    } else if(checkBoxes[0].checked) {
-      return "PREMIUM"
-    } else if(!checkBoxes[0].checked && !checkBoxes[1].checked) {
-      return "NOGARANSI"
+    if (checkBoxes[0].checked && checkBoxes[1].checked) {
+      return "GARANSI_PREMIUM";
+    } else if (checkBoxes[0].checked && !checkBoxes[1].checked) {
+      return "NOGARANSI_PREMIUM";
+    } else if (checkBoxes[1].checked) {
+      return "GARANSI";
+    } else if (checkBoxes[0].checked) {
+      return "PREMIUM";
+    } else if (!checkBoxes[0].checked && !checkBoxes[1].checked) {
+      return "NOGARANSI";
     }
-    
   }
-  const type = bast1AndType()
- 
+  const type = bast1AndType();
+
   const datekRadio = {
-    nearEnd : {
-      actual_ne_idu : val2.length === 0 ? val[1] : "",
-      ne_site_id    : val[5] === undefined ? val2[0][1] : val[5],
-      ne_site_name  : val[9] === undefined ? val2[1][1] : val[9],
-      ne_alamat     : val[13] === undefined ? "" : val[13],
-      ne_latitude   : val[17] === undefined ? "" : val[17],
-      ne_longitude  : val[21] === undefined ? "" : val[21]
+    nearEnd: {
+      actual_ne_idu: val2.length === 0 ? val[1] : "",
+      ne_site_id: val[5] === undefined ? val2[0][1] : val[5],
+      ne_site_name: val[9] === undefined ? val2[1][1] : val[9],
+      ne_alamat: val[13] === undefined ? "" : val[13],
+      ne_latitude: val[17] === undefined ? "" : val[17],
+      ne_longitude: val[21] === undefined ? "" : val[21],
     },
-    farEnd : {
-      actual_fe_idu : val2.length === 0 ? val[3] : "",
-      fe_site_id    : val[7] === undefined ? val2[2][1] : val[7],
-      fe_site_name  : val[11] === undefined ? val2[3][1] : val[11],
-      fe_alamat     : val[15] === undefined ? "" : val[15],
-      fe_latitude   : val[19] === undefined ? "" : val[19],
-      fe_longitude  : val[23] === undefined ? "" : val[23]
-    }
-    
-  }
+    farEnd: {
+      actual_fe_idu: val2.length === 0 ? val[3] : "",
+      fe_site_id: val[7] === undefined ? val2[2][1] : val[7],
+      fe_site_name: val[11] === undefined ? val2[3][1] : val[11],
+      fe_alamat: val[15] === undefined ? "" : val[15],
+      fe_latitude: val[19] === undefined ? "" : val[19],
+      fe_longitude: val[23] === undefined ? "" : val[23],
+    },
+  };
 
   const ticketNumber = `[OPEN]
 
@@ -162,29 +236,38 @@ Ticket No.
 Nossa  : 
 Remedy : 
 
-`
-  let headline = `Headline : `
-  let HEADLINE = `TSEL_${radioBtn[1].checked || radioBtn[2].checked ? `CNQ` : ``}_RADIOIP_${datekRadio.nearEnd.ne_site_id}_${datekRadio.nearEnd.ne_site_name.replace(/\s|\W|\./g, "_")}_TO_${datekRadio.farEnd.fe_site_id}_${datekRadio.farEnd.fe_site_name.replace(/\s|\W|\./g, "_")}_${service}_${type}_REGTSEL${regTsel}`
-  HEADLINE = HEADLINE.replace(/_{2,}/g, "_").toUpperCase()
-  headline = headline+HEADLINE
+`;
+  let headline = `Headline : `;
+  let HEADLINE = `TSEL_${
+    radioBtn[1].checked || radioBtn[2].checked ? `CNQ` : ``
+  }_RADIOIP_${
+    datekRadio.nearEnd.ne_site_id
+  }_${datekRadio.nearEnd.ne_site_name.replace(/\s|\W|\./g, "_")}_TO_${
+    datekRadio.farEnd.fe_site_id
+  }_${datekRadio.farEnd.fe_site_name.replace(
+    /\s|\W|\./g,
+    "_"
+  )}_${service}_${type}_REGTSEL${regTsel}`;
+  HEADLINE = HEADLINE.replace(/_{2,}/g, "_").toUpperCase();
+  headline = headline + HEADLINE;
 
   const ticketDuration = `
 
 #OPEN  : 
 #CLOSE : -
 #TTR   : -
-  `
+  `;
   const bast_1 = `
 BAST-1 : 
 Status : ${val.length === 4 ? "REDEPLOY" : "OOW"}
-  `
+  `;
   const ne = `
 ACTUAL NE IDU : ${datekRadio.nearEnd.actual_ne_idu}
 NE SITE ID    : ${datekRadio.nearEnd.ne_site_id}
 NE SITE NAME  : ${datekRadio.nearEnd.ne_site_name}
 NE ALAMAT     : ${datekRadio.nearEnd.ne_alamat}
 NE LATITUDE   : ${datekRadio.nearEnd.ne_latitude}
-NE LONGITUDE  : ${datekRadio.nearEnd.ne_longitude}`
+NE LONGITUDE  : ${datekRadio.nearEnd.ne_longitude}`;
 
   const fe = `
 
@@ -193,28 +276,31 @@ FE SITE ID    : ${datekRadio.farEnd.fe_site_id}
 FE SITE NAME  : ${datekRadio.farEnd.fe_site_name}
 FE ALAMAT     : ${datekRadio.farEnd.fe_alamat}
 FE LATITUDE   : ${datekRadio.farEnd.fe_latitude}
-FE LONGITUDE  : ${datekRadio.farEnd.fe_longitude}`
+FE LONGITUDE  : ${datekRadio.farEnd.fe_longitude}`;
 
   const info = `
 
 #Info     : Power aman.
-#Action   : Koordinasi dengan rekan PST dan Regional ${regTelkom ? regTelkom : ""}
-#PIC TSEL : `
-  const finalDatek = ticketNumber+headline+ticketDuration+bast_1+ne+fe+info
-  
-  document.getElementById("datek").value = finalDatek
-  calcHeight(finalDatek)
-  let numberOfNewLine = finalDatek.match(/\n/g).length
-  document.querySelector(".textarea-aing").style.height = `${300+(numberOfNewLine*20)+6+1}px`
-   
+#Action   : Koordinasi dengan rekan PST dan Regional ${
+    regTelkom ? regTelkom : ""
+  }
+#PIC TSEL : `;
+  const finalDatek =
+    ticketNumber + headline + ticketDuration + bast_1 + ne + fe + info;
+
+  document.getElementById("datek").value = finalDatek;
+  calcHeight(finalDatek);
+  let numberOfNewLine = finalDatek.match(/\n/g).length;
+  document.querySelector(".textarea-aing").style.height = `${
+    300 + numberOfNewLine * 20 + 6 + 1
+  }px`;
 }
 
 // Dealing with Textarea Height
 function calcHeight(value) {
   let numberOfLineBreaks = (value.match(/\n/g) || []).length;
-  let newHeight = 300 + (numberOfLineBreaks * 20) + 6 + 1;
+  let newHeight = 300 + numberOfLineBreaks * 20 + 6 + 1;
   return newHeight;
-
 }
 
 let textarea = document.querySelector(".textarea-aing");
@@ -222,20 +308,17 @@ textarea.addEventListener("keyup", (event) => {
   if (event.key === "Enter" || event.key === "Backspace") {
     textarea.style.height = calcHeight(textarea.value) + "px";
   }
-
 });
 
-
-console.log(document.querySelector(".textarea-aing").style.height)
+console.log(document.querySelector(".textarea-aing").style.height);
 function reset(id) {
   if (id === "fix") {
     document.getElementById("fix").value = "";
     document.getElementById("fixRes").value = "";
     document.getElementById("inlineRadio6").checked = false;
     document.getElementById("inlineRadio7").checked = false;
-
   } else if (id === "radio") {
-    document.querySelector(".textarea-aing").style.height = '300px';
+    document.querySelector(".textarea-aing").style.height = "300px";
     document.getElementById("radio").value = "";
     document.getElementById("datek").value = "";
     document.getElementById("inlineRadio1").checked = false;
@@ -245,21 +328,17 @@ function reset(id) {
     document.getElementById("RegTsel").options.selectedIndex = 0;
     document.getElementById("flexCheckDefault").checked = false;
     document.getElementById("flexCheckDefault2").checked = false;
-
   } else if (id === "report") {
     document.getElementById("report").value = "\nSALSIM: ";
     document.getElementById("reportRes").value = "";
-
   } else if (id === "RegTsel") {
     document.getElementById("inlineRadio1").checked = false;
     document.getElementById("inlineRadio2").checked = false;
     document.getElementById("inlineRadio3").checked = false;
-  }
-  else if (id === "ipbbran") {
+  } else if (id === "ipbbran") {
     document.getElementById("ipbbran").value = "";
     document.getElementById("ipbbranRes").value = "";
   }
-
 }
 
 function newSeparate(id) {
@@ -267,24 +346,19 @@ function newSeparate(id) {
     let val = document.getElementById("txtRes").value;
     val = val.replace(/,/gm, ";");
     document.getElementById("txtRes").value = val;
-
   } else if (id === "inlineRadio5") {
     let val = document.getElementById("txtRes").value;
     val = val.replace(/;/gm, ",");
     document.getElementById("txtRes").value = val;
-
   } else if (id === "inlineRadio6") {
     let val = document.getElementById("fixRes").value;
     val = val.replace(/,/gm, ";");
     document.getElementById("fixRes").value = val;
-
   } else if (id === "inlineRadio7") {
     let val = document.getElementById("fixRes").value;
     val = val.replace(/;/gm, ",");
     document.getElementById("fixRes").value = val;
-
   }
-
 }
 
 function getSLD() {
@@ -292,40 +366,80 @@ function getSLD() {
 
   let ipbb = [];
   let ipran = [];
-  
-  let ipbb_link = []
-  let ipran_link = []
 
-  let patt1 = /(-ran-|ran-|-ran)/
-  let patt2 = /(core|-sr-|sr-|-sr)/
-  let patt3 = /x\d{2,}g/i
-  let patt4 = /x\d{1}g/i
-  let patt5 = /x\d{3,}mb|m/i
+  let ipbb_link = [];
+  let ipran_link = [];
+
+  let patt1 = /(-ran-|ran-|-ran)/;
+  let patt2 = /(core|-sr-|sr-|-sr)/;
+  let patt3 = /x\d{2,}g/i;
+  let patt4 = /x\d{1}g/i;
+  let patt5 = /x\d{3,}mb|m/i;
 
   for (let i = 0; i < val.length; i++) {
-    if(patt1.test(val[i])) {
-      ipran.push(val[i])
-    } else if(patt2.test(val[i])) {
-      ipbb.push(val[i])
-    } else if(patt3.test(val[i])) {
-      ipbb_link.push(val[i])
-    } else if(patt4.test(val[i])) {
-      ipran_link.push(val[i])
-    } else if(patt5.test(val[i])) {
-      ipran_link.push(val[i])
+    if (patt1.test(val[i])) {
+      ipran.push(val[i]);
+    } else if (patt2.test(val[i])) {
+      ipbb.push(val[i]);
+    } else if (patt3.test(val[i])) {
+      ipbb_link.push(val[i]);
+    } else if (patt4.test(val[i])) {
+      ipran_link.push(val[i]);
+    } else if (patt5.test(val[i])) {
+      ipran_link.push(val[i]);
     }
-
   }
 
-  let interface = ipbb.concat(ipran)
+  let interface = ipbb.concat(ipran);
 
-  const finalResult = `Jumlah Link IPBB & IPRAN: ${(ipbb.length + ipran.length).length === 0 ? "-" : (ipbb.length + ipran.length)}
+  const finalResult = `Jumlah Link IPBB & IPRAN: ${
+    (ipbb.length + ipran.length).length === 0 ? "-" : ipbb.length + ipran.length
+  }
 Core: ${ipbb.length === 0 ? "-" : ipbb.length}
 RAN: ${ipran.length === 0 ? "-" : ipran.length}
 Detail Link:
 IPBB: ${ipbb_link.length === 0 ? "-" : "\n" + ipbb_link}
 IPRAN: ${ipran_link.length === 0 ? "-" : "\n" + ipran_link}
-Interface: ${interface.length === 0 ? "" : "\n" + interface}`
-  document.getElementById("ipbbranRes").value = finalResult.replace(/,/g, "\n")
-  
+Interface: ${interface.length === 0 ? "" : "\n" + interface}`;
+  document.getElementById("ipbbranRes").value = finalResult.replace(/,/g, "\n");
+}
+
+// block number
+const num = `
+081524053
+0816241
+085971717
+087843943
+085654490
+087761212
+081945863
+085925383
+085934526
+087815230
+087778334
+081999852
+081933957
+081933968
+087810684
+087842337
+087860611
+087814093
+087817674
+087844402
+08592729
+083150822
+0856911749
+08385467
+087848216
+087754981
+08385442
+085929647
+0838387
+`;
+
+const res = num.match(/0(\d{7,10})/gi);
+
+for (var i = 0; i < res.length; i++) {
+  var command = `ADD CNACLR: CSC=65534, CID=K'${res[i]}, LP=65534, PFX=K'*65534#, FUNC=REJ;`;
+  console.log(command);
 }
